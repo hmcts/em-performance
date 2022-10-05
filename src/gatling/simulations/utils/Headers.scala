6e1c3846facacb50cf4e7e9080b64c324af8cfe3
craig ritchie <craigritchie@craigs-MBP.broadband>
2022-10-05 17:06:52 +0100

    framwork tidy up for em and initial commit of dm store requests EM-4643

---
+++ src/gatling/simulations/utils/Headers.scala
@@ -0,0 +1,45 @@
+package utils
+import utils.Environment._
+
+
+object Headers {
+
+  //S2S Authentication Header
+  val authenticateS2SPostHeader = Map(
+    "Accept" -> "*/*",
+    "Host" -> rpeHost,
+    "Content-Type" -> "application/json",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Connection" -> "keep-alive"
+  )
+
+  /*DM Store Post Document Header
+  serviceauthorization value obtained from Authentication */
+  val dmStorePostDocumentHeader = Map(
+    "serviceauthorization" -> "${authToken}",
+    "Content-Type" -> "multipart/form-data",
+    "Accept" -> "*/*",
+    "Host" -> dmStoreHost,
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Connection" -> "keep-alive"
+  )
+
+  /*DM Store Get Document Header
+    serviceauthorization, userRole and userId value obtained from Authentication*/
+  val dmStoreGetDocumentHeader = Map(
+    "serviceauthorization" -> "${authToken}",
+    "Accept" -> "*/*",
+    "Host" -> dmStoreHost,
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Connection" -> "keep-alive",
+    "user-roles" -> "${userRole}",
+    "user-id" -> "${userId}",
+
+  )
+
+
+
+
+
+
+}
