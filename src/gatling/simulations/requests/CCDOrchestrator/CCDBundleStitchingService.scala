67c28b5f30772343cd5d437e457bc2fa2e87f107
craig ritchie <craigritchie@craigs-MBP.broadband>
2022-12-20 12:56:22 +0000

    updated commentary on the ccdbundlestitching

--- src/gatling/simulations/requests/CCDOrchestrator/CCDBundleStitchingService.scala
+++ src/gatling/simulations/requests/CCDOrchestrator/CCDBundleStitchingService.scala
@@ -14,7 +14,9 @@ object CCDBundleStitchingService {
 
   /* POST request for creating a document bundle.  The request requires both an S2S token and Idam token and this should be
         called prior to the request being made (the tokens are sent in the header).  A case bundle and new document will be created on
-        successful stitching of the file as this is a synchronous call that will immediately respond with the new document details*/
+        successful stitching of the file as this is a synchronous call that will immediately respond with the new document details
+        The function documentListGenerator will automatically generate the JSON required for the payload - currently set by up to 3
+        documents being stitched with a mixture of pdf and word documents currently being used.*/
 
   val CCDBundleCreateBundleSync =
 
